f5d949d893ebfa276751b92ad875efb6ba936f11
Ivan Timofeev <timofeev.i.s@gmail.com>
2021-05-16 13:42:38 +0400

    copy lesson 23 to lesson 28

---
+++ les28/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import provider.cbr.CbrExchangeRateProvider;
+import servlet.CurrencyExchangeRateServlet;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("", new File(".").getAbsolutePath());
+
+        String servletName = "ExchangeRatesServlet";
+        Tomcat.addServlet(context, servletName, new CurrencyExchangeRateServlet(new CbrExchangeRateProvider()));
+        context.addServletMappingDecoded("/exchange-rates/api", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
